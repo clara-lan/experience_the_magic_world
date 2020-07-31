a1894cf284cc81dcce9241e006ad5e3fcc36c6dc
Clara Lan <clarajylan@gmail.com>
2020-07-31 16:17:44 -0400

    chores

--- src/pages/PeoplePage/PeoplePage.jsx
+++ src/pages/PeoplePage/PeoplePage.jsx
@@ -3,10 +3,10 @@ import { MDBDataTableV5 } from 'mdbreact';
 import "bootstrap/dist/css/bootstrap.css";
 import { Table } from 'react-bootstrap';
 //need to refactor key in product env
-import key from '../../utils/dev';
+ 
 
 function PeoplePage(props){
-  const endpoint=`https://www.potterapi.com/v1/characters?key=${key.API_KEY}`;
+  const endpoint='https://www.potterapi.com/v1/characters?key=$2a$10$ft0mT0WbfKBtwYv/zjSWjeSAUZd/w/dV5I/nUce3u3OnUls05g9N6';
   const [peopleData, setPeopleData] = useState([]);
   const[tableData, setTableData] = useState({});
 
@@ -59,7 +59,7 @@ function PeoplePage(props){
     </tr>
   </thead>
   <tbody>
-    {peopleData.map((char,index)=>(
+    { peopleData.map((char,index)=>(
        <tr>
           <td>{char.name}</td>
           <td>{char.role}</td>
